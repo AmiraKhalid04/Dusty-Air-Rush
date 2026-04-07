dee308a549d731ed7f248d47a337a4f9da15d818
AliElDin Alaa <147806061+AliAlaa88@users.noreply.github.com>
2026-04-07 07:50:28 +0200

    feat: req-1

--- source/common/shader/shader.cpp
+++ source/common/shader/shader.cpp
@@ -20,24 +20,31 @@ bool our::ShaderProgram::attach(const std::string &filename, GLenum type) const 
     const char* sourceCStr = sourceString.c_str();
     file.close();
 
-    //TODO: Complete this function
-    //Note: The function "checkForShaderCompilationErrors" checks if there is
-    // an error in the given shader. You should use it to check if there is a
-    // compilation error and print it so that you can know what is wrong with
-    // the shader. The returned string will be empty if there is no errors.
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &sourceCStr, nullptr);
+    
+    glCompileShader(shader);
+    std::string error = checkForShaderCompilationErrors(shader);
+    if (!error.empty()) {
+        std::cerr << "ERROR: Shader compilation failed for " << filename << ":\n" << error << std::endl;
+        glDeleteShader(shader);
+        return false;
+    }
 
-    //We return true if the compilation succeeded
+    glAttachShader(program, shader);
+    glDeleteShader(shader);
     return true;
 }
 
 
 
 bool our::ShaderProgram::link() const {
-    //TODO: Complete this function
-    //Note: The function "checkForLinkingErrors" checks if there is
-    // an error in the given program. You should use it to check if there is a
-    // linking error and print it so that you can know what is wrong with the
-    // program. The returned string will be empty if there is no errors.
+    glLinkProgram(program);
+    std::string error = checkForLinkingErrors(program);
+    if (!error.empty()) {
+        std::cerr << "ERROR: Shader program linking failed:\n" << error << std::endl;
+        return false;
+    }
 
     return true;
 }
--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -17,10 +17,10 @@ namespace our {
 
     public:
         ShaderProgram(){
-            //TODO: (Req 1) Create A shader program
+            program = glCreateProgram();
         }
         ~ShaderProgram(){
-            //TODO: (Req 1) Delete a shader program
+            glDeleteProgram(program);
         }
 
         bool attach(const std::string &filename, GLenum type) const;
@@ -32,39 +32,40 @@ namespace our {
         }
 
         GLuint getUniformLocation(const std::string &name) {
-            //TODO: (Req 1) Return the location of the uniform with the given name
+            return glGetUniformLocation(program, name.c_str());
         }
 
         void set(const std::string &uniform, GLfloat value) {
-            //TODO: (Req 1) Send the given float value to the given uniform
+            glUniform1f(glGetUniformLocation(program, uniform.c_str()), value);
         }
 
         void set(const std::string &uniform, GLuint value) {
-            //TODO: (Req 1) Send the given unsigned integer value to the given uniform
+            glUniform1ui(glGetUniformLocation(program, uniform.c_str()), value);
         }
 
         void set(const std::string &uniform, GLint value) {
-            //TODO: (Req 1) Send the given integer value to the given uniform
+            glUniform1i(glGetUniformLocation(program, uniform.c_str()), value);
         }
 
         void set(const std::string &uniform, glm::vec2 value) {
-            //TODO: (Req 1) Send the given 2D vector value to the given uniform
+            glUniform2f(glGetUniformLocation(program, uniform.c_str()), value.x, value.y);
         }
 
         void set(const std::string &uniform, glm::vec3 value) {
-            //TODO: (Req 1) Send the given 3D vector value to the given uniform
+            glUniform3f(glGetUniformLocation(program, uniform.c_str()), value.x, value.y, value.z);
         }
 
         void set(const std::string &uniform, glm::vec4 value) {
-            //TODO: (Req 1) Send the given 4D vector value to the given uniform
+            glUniform4f(glGetUniformLocation(program, uniform.c_str()), value.x, value.y, value.z, value.w);
         }
 
         void set(const std::string &uniform, glm::mat4 matrix) {
-            //TODO: (Req 1) Send the given matrix 4x4 value to the given uniform
+            glUniformMatrix4fv(glGetUniformLocation(program, uniform.c_str()), 1, GL_FALSE, glm::value_ptr(matrix));
         }
 
-        //TODO: (Req 1) Delete the copy constructor and assignment operator.
-        //Question: Why do we delete the copy constructor and assignment operator?
+        // to prevent accidental shallow copies of managed pointers
+        ShaderProgram(const ShaderProgram&) = delete;
+        ShaderProgram &operator=(ShaderProgram const &) = delete;
     };
 
 }

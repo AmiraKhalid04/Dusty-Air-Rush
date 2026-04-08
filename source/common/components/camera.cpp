3b776f79591182edf8597b8ec524b63852f11ac8
AliElDin Alaa <147806061+AliAlaa88@users.noreply.github.com>
2026-04-08 07:36:50 +0200

    feat: req-8

--- source/common/components/camera.cpp
+++ source/common/components/camera.cpp
@@ -23,29 +23,24 @@ namespace our {
     glm::mat4 CameraComponent::getViewMatrix() const {
         auto owner = getOwner();
         auto M = owner->getLocalToWorldMatrix();
-        //TODO: (Req 8) Complete this function
-        //HINT:
-        // In the camera space:
-        // - eye is the origin (0,0,0)
-        // - center is any point on the line of sight. So center can be any point (0,0,z) where z < 0. For simplicity, we let center be (0,0,-1)
-        // - up is the direction (0,1,0)
-        // but to use glm::lookAt, we need eye, center and up in the world state.
-        // Since M (see above) transforms from the camera to thw world space, you can use M to compute:
-        // - the eye position which is the point (0,0,0) but after being transformed by M
-        // - the center position which is the point (0,0,-1) but after being transformed by M
-        // - the up direction which is the vector (0,1,0) but after being transformed by M
-        // then you can use glm::lookAt
-        return glm::mat4(1.0f);
+        glm::vec3 eye = glm::vec3(M[3]);
+        glm::vec3 center = glm::vec3(M[3]) - glm::vec3(M[2]);
+        glm::vec3 up = glm::vec3(M[1]);
+        return glm::lookAt(eye, center, up);
     }
 
     // Creates and returns the camera projection matrix
     // "viewportSize" is used to compute the aspect ratio
     glm::mat4 CameraComponent::getProjectionMatrix(glm::ivec2 viewportSize) const {
-        //TODO: (Req 8) Wrtie this function
-        // NOTE: The function glm::ortho can be used to create the orthographic projection matrix
-        // It takes left, right, bottom, top. Bottom is -orthoHeight/2 and Top is orthoHeight/2.
-        // Left and Right are the same but after being multiplied by the aspect ratio
-        // For the perspective camera, you can use glm::perspective
-        return glm::mat4(1.0f);
+        float aspect = (float)viewportSize.x / (float)viewportSize.y;
+        if(cameraType == CameraType::ORTHOGRAPHIC){
+            float top = orthoHeight / 2.0f;
+            float bottom = -top;
+            float right = top * aspect;
+            float left = -right;
+            return glm::ortho(left, right, bottom, top, near, far);
+        } else {
+            return glm::perspective(fovY, aspect, near, far);
+        }
     }
 }
--- source/common/ecs/world.hpp
+++ source/common/ecs/world.hpp
@@ -24,9 +24,10 @@ namespace our {
         // to put it in the "markedForRemoval" set. The elements in the "markedForRemoval" set will be removed and
         // deleted when "deleteMarkedEntities" is called.
         Entity* add() {
-            //TODO: (Req 8) Create a new entity, set its world member variable to this,
-            // and don't forget to insert it in the suitable container.
-            return nullptr;
+            Entity* entity = new Entity();
+            entity->world = this;
+            entities.insert(entity);
+            return entity;
         }
 
         // This returns and immutable reference to the set of all entites in the world.
@@ -37,18 +38,28 @@ namespace our {
         // This marks an entity for removal by adding it to the "markedForRemoval" set.
         // The elements in the "markedForRemoval" set will be removed and deleted when "deleteMarkedEntities" is called.
         void markForRemoval(Entity* entity){
-            //TODO: (Req 8) If the entity is in this world, add it to the "markedForRemoval" set.
+            if(entity->world == this){
+                markedForRemoval.insert(entity);
+            }
         }
 
         // This removes the elements in "markedForRemoval" from the "entities" set.
         // Then each of these elements are deleted.
         void deleteMarkedEntities(){
-            //TODO: (Req 8) Remove and delete all the entities that have been marked for removal
+            for(Entity* entity : markedForRemoval){
+                entities.erase(entity);
+                delete entity;
+            }
+            markedForRemoval.clear();
         }
-
+        
         //This deletes all entities in the world
         void clear(){
-            //TODO: (Req 8) Delete all the entites and make sure that the containers are empty
+            for(Entity* entity : entities){
+                delete entity;
+            }
+            markedForRemoval.clear();
+            entities.clear();
         }
 
         //Since the world owns all of its entities, they should be deleted alongside it.

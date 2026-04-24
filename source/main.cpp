e0571802c543f36017f4284d2e16b3ac9f587853
AmiraKhalid04 <amirakhaled928@gmail.com>
2026-04-24 17:54:21 +0300

    feat: add game over screen

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@
 #include <application.hpp>
 
 #include "states/menu-state.hpp"
+#include "states/loss-state.hpp"
 #include "states/play-state.hpp"
 #include "states/shader-test-state.hpp"
 #include "states/mesh-test-state.hpp"
@@ -43,6 +44,7 @@ int main(int argc, char** argv) {
     
     // Register all the states of the project in the application
     app.registerState<Menustate>("menu");
+    app.registerState<LossState>("loss");
     app.registerState<Playstate>("play");
     app.registerState<ShaderTestState>("shader-test");
     app.registerState<MeshTestState>("mesh-test");

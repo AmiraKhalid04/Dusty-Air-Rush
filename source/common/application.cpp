6db145b3f4bdd7b7011351d84938edf30fee55b3
“OmarNabil005” <omarnabil219@gmail.com>
2026-05-01 20:37:06 +0300

    refactor: remove duplicate line to avoid build warning

--- source/common/application.cpp
+++ source/common/application.cpp
@@ -13,7 +13,6 @@
 #include <flags/flags.h>
 
 // Include the Dear ImGui implementation headers
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD2
 #include <imgui_impl/imgui_impl_glfw.h>
 #include <imgui_impl/imgui_impl_opengl3.h>
 
